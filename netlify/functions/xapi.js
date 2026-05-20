7c99ae0ce31437f91152b1961c35476f4bfe0d51
9ggqkr89p4-boop <9ggqkr89p4@privaterelay.appleid.com>
2026-05-20 10:25:24 +0300

    Update xapi.js

--- netlify/functions/xapi.js
+++ netlify/functions/xapi.js
@@ -1,3 +1,4 @@
+// netlify/functions/xapi.js
 exports.handler = async function(event, context) {
   const BEARER = "AAAAAAAAAAAAAAAAAAAAALeV9gEAAAAAkhB06nMro1L2nc3wGjpOUWIedR8%3DOkiw4SDQ8F4j4FgyNweykdB5YEhynVgwH2F1jmrjgKQnwTwyik";
   const HANDLES = "zbriefkani,haidari_ii,K992Zhraa,shang_salar,fenk24,adamrizgar,mohammed_fayeqA,ahmed_hazharr,safinbarzany";
@@ -5,8 +6,16 @@ exports.handler = async function(event, context) {
     const url = "https://api.twitter.com/2/users/by?usernames="+HANDLES+"&user.fields=public_metrics,profile_image_url,description,name";
     const res = await fetch(url, {headers:{"Authorization":"Bearer "+BEARER}});
     const data = await res.json();
-    return {statusCode:200,headers:{"Content-Type":"application/json","Access-Control-Allow-Origin":"*"},body:JSON.stringify(data)};
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify(data)
+    };
   } catch(e) {
-    return {statusCode:500,headers:{"Access-Control-Allow-Origin":"*"},body:JSON.stringify({error:e.message})};
+    return {
+      statusCode: 500,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({ error: e.message })
+    };
   }
 };
